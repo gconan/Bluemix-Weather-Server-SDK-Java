ac5f6f906c4114c6a7f82a2167adff01689dae19
Conan Gammel <Cgammel@Conans-MacBook-Pro-2.local>
2016-08-02 15:58:39 -0500

    added first files

---
+++ Bluemix-Weather-Server-SDK-Java/src/weather/server/sdk/v1/Weather.java
@@ -0,0 +1,5 @@
+package weather.server.sdk.v1;
+
+public class Weather {
+
+}
---
+++ Bluemix-Weather-Server-SDK-Java/test/weather/server/sdk/v1/WeatherTest.java
@@ -0,0 +1,14 @@
+package weather.server.sdk.v1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WeatherTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
